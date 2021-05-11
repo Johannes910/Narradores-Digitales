c612534d19c34ae9617581804065b15c055dd38b
Johannes Herzog-Ahorn <48024528+Johannes910@users.noreply.github.com>
2021-05-10 19:11:46 -0500

    Comentarios

--- PROYECTO FINAL/Html/app.js
+++ PROYECTO FINAL/Html/app.js
@@ -2,6 +2,7 @@ const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 
+//Metodo ppara añadir un boton con click
 sign_up_btn.addEventListener('click',() =>{
 	container.classList.add("sign-up-mode");
 });
