e5b5eaa231c1a071a90dd5f745a05902533131b7
Juan Cortés <juan.cortns@uao.edu.co>
2021-05-23 21:19:05 -0500

    Boton para grabar y finalizar
    B4

--- PROYECTO FINAL/Html/Arduino.js
+++ PROYECTO FINAL/Html/Arduino.js
@@ -2,9 +2,23 @@ let dato;
 let contador1 = 0;
 let contador2 = 0;
 let contador3 = 0;
+let contador4 = 0;
 var serial;
 var port = 'COM5'; // variable que indica el puerto serial utilizado por el Arduino
 var backgroundColor = 'rgb(0, 0, 0)'; // variable donde se almacena el color de fondo
+
+/// GODSENT //////////////
+let cwidth = 640;
+let cheight = 360;
+let button;
+let encoder;
+const frate = 30; // frame rate
+const numFrames = 0; // num of frames to record
+let recording = false;
+let recordedFrames = 0;
+let count = 0;
+let stopRecording = false;
+
 
 function setup() {
  createCanvas(windowWidth, windowHeight);
@@ -21,6 +35,9 @@ function setup() {
 
  // Determina el método que se llama cuando hay datos en el puerto
  serial.on('data', getData);
+
+//  button = createButton('record')
+//  button.mousePressed(() => recording = true)
 }
 
 
@@ -42,14 +59,40 @@ function getData() {
    dato = data;
 }
 
-
-
 function draw() 
 {
     drawScreen();
 
+    // keep adding new frame
+    if (recording) {
+        console.log('recording')
+        encoder.addFrameRgba(drawingContext.getImageData(0, 0, encoder.width, encoder.height).data);
+        recordedFrames++
+    }
+    
+
 }
 
+function StopRecording() 
+{
+    if(recordedFrames > numFrames)
+    {
+        recording = false
+        recordedFrames = 0
+        console.log('recording stopped')
+
+        encoder.finalize();
+        const uint8Array = encoder.FS.readFile(encoder.outputFilename);
+        const anchor = document.createElement('a')
+        anchor.href = URL.createObjectURL(new Blob([uint8Array], { type: 'video/mp4' }))
+        anchor.download = encoder.outputFilename
+        anchor.click()
+        encoder.delete()
+
+        preload() // reinitialize encoder
+        final = false;
+    }
+}
 
 class Personaje {
     constructor(posX,posY,tam){
@@ -97,6 +140,11 @@ class Rectangle extends Personaje {
                     dato = '0';
                 }
             break;
+
+            case '4':
+                contador4++;
+                dato = '0';
+            break;
         }
 
     if(contador1 % 2 !== 0)
@@ -107,6 +155,14 @@ class Rectangle extends Personaje {
     if(contador2 % 2 !== 0 )
     {
         this.posX=this.posX-this.speed;
+    }
+
+    if(contador4 % 2 !== 0)
+    {
+        recording = true;
+    }else
+    {
+        StopRecording();
     }
 
     if(this.posX > width-this.tam)
@@ -126,12 +182,25 @@ let personaje = new Rectangle(320, 180, 25);
 
 function drawScreen()
 {
-createCanvas(640,360);   
+createCanvas(cwidth,cheight);   
  background(232,120,146);
  rectMode(CENTER);
  fill(255);
  personaje.draw();
  personaje.move();
+}
+
+function preload() {
+    HME.createH264MP4Encoder().then(enc => {
+        encoder = enc;
+        encoder.outputFilename = 'test';
+        encoder.width = cwidth*2;
+        encoder.height = cheight*2;
+        encoder.frameRate = frate;
+        encoder.kbps = 50000; // video quality
+        encoder.groupOfPictures = 10; // lower if you have fast actions.
+        encoder.initialize()
+    })
 }
 
 
